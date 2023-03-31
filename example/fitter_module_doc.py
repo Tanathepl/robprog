5d064a42450592ec62c106db0437fc1a1aacecc5
JulienPeloton <peloton@lal.in2p3.fr>
2023-03-31 17:16:10 +0200

    Remove unused code

--- example/fitter_module_doc.py
+++ example/fitter_module_doc.py
@@ -184,15 +184,6 @@ def plot_fit(
     plt.legend()
     plt.show()
 
-def main():
-    fn = sys.argv[1]
-
-    data = load_data(fn)
-
-    params, errors = fit_data(data['x'], data['y'])
-
-    plot_fit(data['x'], data['y'], params, errors)
-
 
 if __name__ == "__main__":
     """ Run the unit test suite """
